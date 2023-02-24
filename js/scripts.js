fdfeb75ca5b9b7baa91121cb889d92660439908d
omar-mFarrag <omar.mustafa.ff@gmail.com>
2023-02-24 15:43:43 +0200

    kd

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 var cart = document.getElementById("cart");
 var cartContent = document.getElementById("cart-content");
 var count = document.getElementById("count");
+var prodInCart = document.getElementById("prod-in-cart");
+var cartNotic = document.getElementById("cart-notic");
 
 var cartClickCounter = 1;
 var counter = 0;
@@ -32,3 +34,44 @@ function plusCounter(){
     counter++;
     count.innerHTML = counter;
 }
+
+function addToCart(){
+    if(counter > 0){
+        cartNotic.style.opacity = 1;
+        cartNotic.innerHTML = counter;
+
+        prodInCart.innerHTML = 
+        `
+            <div class="prod-in-cart">
+                <img src="./images/image-product-1-thumbnail.jpg" alt="">
+                <div class="prod-in-cart-info">
+                    <p>Fall Limited Edition Sneakers</p>
+                    <div class="total-price">
+                        <span>$125.00 x ${counter}</span>
+                        <span>$${counter * 125}.00</span>
+                    </div>
+                </div>
+                <img onclick="deletCart();" src="./images/icon-delete.svg" alt="">
+            </div>
+            <button>Checkout</button>
+        `;
+    }else if(counter === 0){
+        prodInCart.innerHTML = `
+        <p>Your cart is empty.</p>
+        `;
+
+        cartNotic.style.opacity = 0;
+        cartNotic.innerHTML = counter;
+    }
+
+
+}
+
+function deletCart(){
+    prodInCart.innerHTML = `<p>Your cart is empty.</p>`;
+    counter = 0;
+    count.innerHTML = counter;
+    cartNotic.style.opacity = 0;
+    cartNotic.innerHTML = counter;
+}
+
